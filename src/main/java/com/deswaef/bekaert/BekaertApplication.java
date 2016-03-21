f92ce3e60145639881b7cd4b44668ecb887e3532
Quinten De Swaef <quintendeswaef@gmail.com>
2016-03-21 11:39:05 +0100

    initial commit

---
+++ src/main/java/com/deswaef/bekaert/BekaertApplication.java
@@ -0,0 +1,47 @@
+package com.deswaef.bekaert;
+
+import com.deswaef.bekaert.service.FileService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+@SpringBootApplication
+public class BekaertApplication implements CommandLineRunner {
+
+    private static final String OUTPUT_FILE_DIRECTORY = "output";
+    @Autowired
+    private FileService fileService;
+
+    private Log LOG = LogFactory.getLog(BekaertApplication.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<File> files = fileService.getFiles();
+        files
+                .forEach(x -> {
+                    if (!x.exists()) {
+                        LOG.info(x + " does not exist in the input directory");
+                    } else {
+                        File file = new File(OUTPUT_FILE_DIRECTORY + File.separator + x.getName());
+                        try {
+                            Files.copy(x.toPath(), file.toPath());
+                        } catch (IOException e) {
+                            LOG.info("error trying to copy " + x.getName() + " > Probably already exists in the output directory");
+                        }
+                    }
+                });
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BekaertApplication.class, args);
+    }
+}
---
+++ src/main/java/com/deswaef/bekaert/service/FileService.java
@@ -0,0 +1,49 @@
+package com.deswaef.bekaert.service;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class FileService {
+
+    private static final String INPUT_FILE_DIRECTORY = "input";
+    private static final String INPUT_FILE = "input.txt";
+
+    private Log LOG = LogFactory.getLog(FileService.class);
+
+    public List<File> getFiles() {
+        System.out.println("going to look for files of 2015");
+        File file = new File(INPUT_FILE);
+        if (file.exists()) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(file));
+                return br.lines()
+                        .map(line -> new File(INPUT_FILE_DIRECTORY + File.separator + line))
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new ArrayList<>();
+            } finally {
+                try {
+                    if (br != null) br.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } else {
+            System.out.println("output file does not exist");
+            return new ArrayList<>();
+        }
+    }
+
+}
